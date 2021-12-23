bbe30ca800340659c6a7c474c251c2226172e18e
Nezuko Sensei <linhvipfcnb02@gmail.com>
2021-12-23 13:36:53 +0700

    commit

--- libs/js/script.js
+++ libs/js/script.js
@@ -8,3 +8,15 @@ const togglePassword = document.querySelector('#togglePassword');
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
+
+
+const togglePassword1 = document.querySelector('#togglePassword1');
+  const password1 = document.querySelector('#id_password1');
+ 
+  togglePassword1.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password1.getAttribute('type') === 'password' ? 'text' : 'password';
+    password1.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
